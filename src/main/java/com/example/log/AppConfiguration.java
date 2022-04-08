349a8bf9ea73a6741cca450baf1b2567f40144a9
shameli <shamelikumarcr7@gmail.com>
2022-04-08 15:08:59 +0530

    log

--- src/main/java/com/example/log/AppConfiguration.java
+++ src/main/java/com/example/log/AppConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppConfiguration {
+    
     @RequestMapping("/logging")
     public String hi()
     {
